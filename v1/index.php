5044b70fca6dbe8a5dbd2be9663df21e966a2b41
underc0de <iegormundo@hotmail.com>
2014-10-13 00:49:03 +0200

    v0.0.2

--- v1/index.php
+++ v1/index.php
@@ -1,4 +1,6 @@
 <?php
+require_once './handlers/application.php';
+require_once './handlers/database.php';
 require '../vendor/autoload.php';
 
 \Slim\Slim::registerAutoloader();
@@ -6,11 +8,31 @@ require '../vendor/autoload.php';
 $app = new \Slim\Slim();
 
 /*!
+ * Application routes
+ */
+
+# Get
+$app->get('/:route', 'about')->conditions(array('route' => '(|about)'));
+
+# Error pages
+$app->notFound(function() {
+    parse(404, array('status' => 404));
+});
+
+$app->error(function (\Exception $e) use ($app) {
+	$response = array();
+	$response['status'] = 500;
+	$response['message'] = $e->getMessage();
+
+	parse($response['status'], $response);
+});
+
+/*!
  * Methods without authentication
  */
-$app->get('/', function() use ($app) {
+function about() {
 	parse(200, "API RESTful v1");
-});
+}
 
 /*!
  * Parse the response to json
